b09c20f3fc11354e612a6944b5615452d37ab41f
AmberWebb <amberrw@yahoo.com>
2019-10-28 16:52:53 -0700

    Satrted on index.handllebars. Got functions to work as well.

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,16 @@
 var connection = require('./connection').default
 require('dotenv').config()
 
-function selectAll (){
+module.exports.select =function selectAll (){
     connection.query('SELECT * FROM burgers;', function (error, results, fields) {
     if (error) throw error 
     console.log(results)
-    return
+    return "The Works"
 })
         
 } 
  
-function insertOne (burgerType){
+module.exports.insert =function insertOne (burgerType){
     connection.query('insert into burgers (burger_name,devoured) values ("' + burgerType +'",false);', function (error, resuls, fields){
         if (error) throw error
         return
@@ -19,7 +19,7 @@ function insertOne (burgerType){
 
 }
 
-function updateOne (burgerID){
+module.exports.devour =function updateOne (burgerID){
     connection.query('update burgers set devoured = true where id='+ burgerID +';', function (error, resuls, fields){
         if (error) throw error
         return
@@ -27,13 +27,4 @@ function updateOne (burgerID){
     })
 
 }
-
-selectAll()
-
-insertOne("mcRib")
-insertOne("bigMac")
-updateOne(1)
-updateOne(2)
-updateOne(3)
-
-selectAll()
+module.exports.select()
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+const path = require('path');
+var orm =  require("./config/orm")   
+ 
+var app = express();
+
+var hbs = exphbs.create({
+
+    
+    helpers: {
+        select: orm.select,
+        insert: orm.insert,
+        devour: orm.devour,
+    }
+});
+ 
+ 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+const publicPath = path.join(__dirname, './views');
+app.use('/', express.static(publicPath));
+    
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(8080);
+
