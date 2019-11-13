6c6f7affe9c4820e44be58e0ff0d1e920a7e051c
AmberWebb <amberrw@yahoo.com>
2019-11-13 08:38:13 -0800

    Wrapped up code, stasrted on styling. Deploying Now.

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,17 @@
 var connection = require('./connection').default
 require('dotenv').config()
 
-module.exports.select =function selectAll (){
+module.exports.select =function selectAll (callback){
     connection.query('SELECT * FROM burgers;', function (error, results, fields) {
     if (error) throw error 
-    console.log(results)
-    return "The Works"
+    callback(results)
+    return 
 })
         
 } 
  
 module.exports.insert =function insertOne (burgerType){
+    if (burgerType=="") return;
     connection.query('insert into burgers (burger_name,devoured) values ("' + burgerType +'",false);', function (error, resuls, fields){
         if (error) throw error
         return
@@ -27,4 +28,3 @@ module.exports.devour =function updateOne (burgerID){
     })
 
 }
-module.exports.select()
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 const path = require('path');
-var orm =  require("./config/orm")   
+var orm =  require("./config/orm")  
+const bodyParser = require("body-parser");
+
  
 var app = express();
 
-var hbs = exphbs.create({
+app.use(bodyParser.urlencoded({ extended: false }));
 
-    
+
+var hbs = exphbs.create({
+    helpers: {
+        devour: orm.devour
+    }
 });
  
  
@@ -19,7 +25,44 @@ const publicPath = path.join(__dirname, './views');
 app.use('/', express.static(publicPath));
     
 app.get('/', function (req, res) {
-    res.render('index');
+    showburgers = function(results){
+        res.render('index',{
+            burgerList: results
+        });
+    }
+    orm.select(showburgers)
+});
+
+app.post("/submit",function (req, res) {
+
+    var burger = req.body.BurgerName
+
+    console.log (burger) 
+
+    orm.insert (burger)
+    
+    showburgers = function(results){
+        res.render('index',{
+            burgerList: results
+        });
+    }
+    orm.select(showburgers)
+
+});
+
+app.post("/click",function (req, res) {
+
+    var burger_id = req.body.id
+
+    orm.devour (burger_id)
+    
+    showburgers = function(results){
+        res.render('index',{
+            burgerList: results
+        });
+    }
+    orm.select(showburgers)
+
 });
 
 app.listen(8080);
